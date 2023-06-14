09fef18a887c10567ab23f96df4951048aef88cf
Daniel Polách <daniel.polach@phrase.com>
2023-06-14 14:56:46 +0200

    ChatbotService

---
+++ src/main/java/com/memsource/hackaton/llmappliedontm/infrastructure/openai/ChatbotService.java
@@ -0,0 +1,18 @@
+package com.memsource.hackaton.llmappliedontm.infrastructure.openai;
+
+
+import com.memsource.hackaton.llmappliedontm.domain.dataset.entity.Dataset;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ChatbotService {
+
+    public Dataset vaporiseDataset(String prompt, Dataset dataset) {
+        return null;
+    }
+
+}

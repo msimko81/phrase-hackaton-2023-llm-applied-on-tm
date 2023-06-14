d06f5f02b808d387d37fe1ff9eef2d8da0cae072
Miroslav Šimko <miroslav.simko@phrase.com>
2023-06-14 12:19:14 +0200

    foo test

---
+++ src/test/java/com/memsource/hackaton/llmappliedontm/FooTest.java
@@ -0,0 +1,32 @@
+package com.memsource.hackaton.llmappliedontm;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.model.Model;
+import com.theokanning.openai.service.OpenAiService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+@Slf4j
+public class FooTest {
+
+    @Autowired
+    OpenAiService openAiService;
+
+    @Test
+    void bar() {
+        List<Model> models = openAiService.listModels();
+        log.info("{}", models);
+
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .prompt("Replace miro by petr in the following text: hello miro")
+                .model("davinci")
+                .echo(true)
+                .build();
+        openAiService.createCompletion(completionRequest).getChoices().forEach(System.out::println);
+    }
+}

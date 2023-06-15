651b59c6cfe52bdcd120cb1df75c91ddb4cb5576
Petr Šimek <petr.simek@phrase.com>
2023-06-15 10:37:31 +0200

    Fix tests

--- src/test/java/com/memsource/hackaton/llmappliedontm/dataset/DatasetControllerTest.java
+++ src/test/java/com/memsource/hackaton/llmappliedontm/dataset/DatasetControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -63,7 +64,7 @@ class DatasetControllerTest {
 
     @Test
     void returnsVaporisedDatasetAfterApplyingPrompt() throws Exception {
-        when(chatbotService.vaporiseDataset(any(), any(), "text-davinci-003", 1)).thenReturn(
+        when(chatbotService.vaporiseDataset(any(), any(), any(), anyInt())).thenReturn(
                 Dataset.builder()
                         .name("vaporised dataset")
                         .segments(
